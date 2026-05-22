8c7d6110c8dd1d2f002b491f865a19df3dabf83b
Tridib Banik <tridib.perfect@gmail.com>
2026-05-22 16:03:38 -0400

    ✅ Validating Credit Card Numbers [python3] — solution

---
+++ solutions/python3/medium/validating-credit-card-number/solution.py
@@ -0,0 +1,28 @@
+# ──────────────────────────────────────────────────
+# Problem     Validating Credit Card Numbers
+# Difficulty  Medium
+# Subdomain   Regex and Parsing
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-05-22, 04:03 p.m.
+# ──────────────────────────────────────────────────
+
+import re
+
+def valid_card(number):
+    if re.fullmatch(r'\d{4}-\d{4}-\d{4}-\d{4}',number):
+        number = number.replace("-","")
+    if not re.fullmatch(r'[456]\d{15}',number):
+        return False
+    if re.search(r'(\d)\1{3,}',number):
+        return False
+    return True
+    
+N = int(input())
+for _ in range(N):
+    number = input().strip()
+    if valid_card(number):
+        print("Valid")
+    else:
+        print("Invalid")
