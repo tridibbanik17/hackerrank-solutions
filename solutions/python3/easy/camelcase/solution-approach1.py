c675da8bdf01b7bb67943029291666eeb8f4a74c
Tridib Banik <tridib.perfect@gmail.com>
2026-05-24 17:48:17 -0400

    ✅ CamelCase [python3] — approach1

---
+++ solutions/python3/easy/camelcase/solution-approach1.py
@@ -0,0 +1,46 @@
+# ──────────────────────────────────────────────────
+# Problem     CamelCase
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-05-24, 05:47 p.m.
+# Time        O(n)
+# Space       O(1)
+# Trick       CamelCase words are delimited by uppercase letters, so count the number of uppercase characters and add one.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    num_of_words = 1
+    # Write your code here
+    for i in range(len(s)):
+        if ord(s[i]) >=65 and ord(s[i]) <= 90:
+            num_of_words += 1
+    return num_of_words
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
