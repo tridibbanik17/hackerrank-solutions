90c783f8a49eda20dd4cf78edb630a7f7597d8ec
Tridib Banik <tridib.perfect@gmail.com>
2026-05-24 16:47:24 -0400

    ✅ Alternating Characters  [python3] — solution

---
+++ solutions/python3/easy/alternating-characters/solution.py
@@ -0,0 +1,49 @@
+# ──────────────────────────────────────────────────
+# Problem     Alternating Characters 
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-05-24, 04:47 p.m.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+    num_of_deletions = 0
+    s_list = list(s)
+    if len(s_list) == 1 or len(s_list) == 0:
+        return 0
+    for i in range(1, len(s_list)):
+        if s_list[i] == s_list[i-1]:
+            num_of_deletions += 1
+    return num_of_deletions
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
