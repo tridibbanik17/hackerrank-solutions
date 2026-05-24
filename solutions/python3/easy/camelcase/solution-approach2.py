033b92d85084d7bc4a0ab45df6f5af0870381c6e
Tridib Banik <tridib.perfect@gmail.com>
2026-05-24 17:53:47 -0400

    ✅ CamelCase [python3] — approach2

---
+++ solutions/python3/easy/camelcase/solution-approach2.py
@@ -0,0 +1,41 @@
+# ──────────────────────────────────────────────────
+# Problem     CamelCase
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-05-24, 05:53 p.m.
+# Time        O(n)
+# Space       O(1)
+# Trick       The number of words in CamelCase is always one plus the count of uppercase letters.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    return 1 + sum(c.isupper() for c in s)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
