086171908ad36045232430f5b92c61806da9aba9
Tridib Banik <tridib.perfect@gmail.com>
2026-05-25 16:01:44 -0400

    ✅ Solve Me First [python3] — approach1

---
+++ solutions/python3/uncategorized/solve-me-first/solution-approach1.py
@@ -0,0 +1,20 @@
+# ──────────────────────────────────────────────────
+# Problem     Solve Me First
+# Difficulty  N/A
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-05-25, 04:01 p.m.
+# ──────────────────────────────────────────────────
+
+
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
