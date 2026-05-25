cfcde3c1d6a4a4d8b2db9bb3fe8e048b8246d51b
Tridib Banik <tridib.perfect@gmail.com>
2026-05-25 16:09:06 -0400

    ✅ Simple Array Sum [python3] — approach1

---
+++ solutions/python3/uncategorized/simple-array-sum/solution-approach1.py
@@ -0,0 +1,40 @@
+# ──────────────────────────────────────────────────
+# Problem     Simple Array Sum
+# Difficulty  N/A
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-05-25, 04:09 p.m.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import os
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    array_sum = 0
+    # Write your code here
+    for element in ar:
+        array_sum += element
+    return array_sum
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
