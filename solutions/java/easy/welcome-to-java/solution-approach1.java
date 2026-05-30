5c4bf773c59303be355aab5c79b6512204a2f7e2
Tridib Banik <tridib.perfect@gmail.com>
2026-05-29 23:34:17 -0400

    ✅ Welcome to Java! [java] — approach1

---
+++ solutions/java/easy/welcome-to-java/solution-approach1.java
@@ -0,0 +1,18 @@
+// ──────────────────────────────────────────────────
+// Problem     Welcome to Java!
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-05-29, 11:34 p.m.
+// ──────────────────────────────────────────────────
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
