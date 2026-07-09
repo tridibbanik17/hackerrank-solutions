bfb8ee9b790ade35231b0ed8f126d588fbad9b7d
Tridib Banik <tridib.perfect@gmail.com>
2026-07-08 20:14:45 -0400

    ✅ Is This a Binary Search Tree? [python3] — approach1

---
+++ solutions/python3/medium/is-binary-search-tree/solution-approach1.py
@@ -0,0 +1,28 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/is-binary-search-tree/problem?isFullScreen=true
+# Problem     Is This a Binary Search Tree?
+# Difficulty  Medium
+# Subdomain   Trees
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-08, 08:14 p.m.
+# ──────────────────────────────────────────────────
+
+
+
+""" Node is defined as
+class node:
+  def __init__(self, data):
+      self.data = data
+      self.left = None
+      self.right = None
+"""
+def check_binary_search_tree_(root, min_val=float('-inf'), max_val=float('inf')):
+    if root is None:
+        return True
+    if root.data <= min_val or root.data >= max_val:
+        return False
+    return (check_binary_search_tree_(root.left, min_val, root.data) and
+            check_binary_search_tree_(root.right, root.data, max_val))
+        
