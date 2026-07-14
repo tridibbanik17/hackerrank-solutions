6f4ab4e07a348bc97d20da52982ed6f974c2c375
Tridib Banik <tridib.perfect@gmail.com>
2026-07-14 12:44:55 -0400

    ✅ Arithmetic Operators [python3] — approach1

---
+++ solutions/python3/easy/python-arithmetic-operators/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+# Problem     Arithmetic Operators
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-14, 12:44 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
